6ee63d5fffb635416b79f6569dc05ffdb99cae38
wjpsoftwarealex <51321599+wjpsoftwarealex@users.noreply.github.com>
2020-10-28 19:15:08 +0000

    Router class added with access to schedule and authors json data
    htaccess added for homepage redirect
    css added

--- classes/jsonpage.class.php
+++ classes/jsonpage.class.php
@@ -6,102 +6,136 @@
 * 
 */
 class JSONpage {
-private $page; 
-private $recordset;
+  private $page; 
+  private $recordset;
 
-/**
-* @param $pathArr - an array containing the route information
-*/
-//THIS IS WHERE I PUT THE ROUTING FROM LAST YEAR
-public function __construct($pathArr, $recordset) {
-  $this->recordset = $recordset;
-  $path = (empty($pathArr[1])) ? "api" : $pathArr[1];
+  /**
+  * @param $pathArr - an array containing the route information
+  */
+  //THIS IS WHERE I PUT THE ROUTING FROM LAST YEAR
+  public function __construct($pathArr, $recordset) {
+    $this->recordset = $recordset;
+    
+    if($pathArr[0] == "api"){
+        if(isset($pathArr[1])){
+            if($pathArr[1] == "schedule"){
+                if(isset($pathArr[2])){
+                  if(isset($pathArr[3])){
 
-  switch ($path) {
-    case 'api':
-      $this->page = $this->json_welcome();
-      break;
-    case 'actors':
-      $this->page = $this->json_actors();
-      break;
-    case 'films':
-      $this->page = $this->json_films();
-      break;
-    default:
-      $this->page = $this->json_error();
-      break;
-  }
-}
+                  }
+                  else{
+                    $this->page = $this->json_schedule($pathArr[2]);
+                  }
+                  
+                }
+                else{
+                  $this->page = $this->json_schedule();
+                }
+            }
+            elseif($pathArr[1] == "session"){
 
-//an arbitrary max length of 20 is set
-private function sanitiseString($x) {
-  return substr(trim(filter_var($x, FILTER_SANITIZE_STRING)), 0, 20);
-}
+            }
+            elseif($pathArr[1]== "authors"){
+                if(isset($pathArr[2])){
+                  if(is_numeric($pathArr[2])){
 
-//an arbitrary max range of 1000 is set
-private function sanitiseNum($x) {
-  return filter_var($x, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>0, "max_range"=>1000)));
-}
-
-private function json_welcome() {
-  $msg = array("message"=>"welcome", "author"=>"Alex Tuersley");
-  return json_encode($msg);
-}
-
-private function json_error() {
-  $msg = array("message"=>"error");
-  return json_encode($msg);
-}
-
-private function json_actors() {
-    $query  = "SELECT first_name, last_name FROM actor";
-    $params = [];
- 
-    if (isset($_REQUEST['search'])) {
-        $query .= " WHERE last_name LIKE :term";
-        $term = $this->sanitiseString("%".$_REQUEST['search']."%");
-        $params = ["term" => $term];
-    }else {
-        if (isset($_REQUEST['id'])) {
-          $query .= " WHERE actor_id = :term";
-          $term = $this->sanitiseNum($_REQUEST['id']);
-          $params = ["term" => $term];
+                  }
+                  else{
+                    $this->page = $this->json_authors($pathArr[2]);
+                  }
+                  
+                }
+                else{
+                  $this->page = $this->json_authors();
+                }
+                
+            }
+            elseif($pathArr[1] == "update"){
+    
+            }
+            elseif($pathArr[1] == "login"){
+    
+            }
+          
+        }
+        else{
+          echo "api";
         }
     }
-    if (isset($_REQUEST['page'])) {
-        $query .= " ORDER BY last_name";
-        $query .= " LIMIT 10 ";
-        $query .= " OFFSET ";
-        $query .= 10 * ($this->sanitiseNum($_REQUEST['page'])-1);
+    else{
+      $this->page = $this->json_error();
+    }
+
+  }
+
+  //an arbitrary max length of 20 is set
+  private function sanitiseString($x) {
+    return substr(trim(filter_var($x, FILTER_SANITIZE_STRING)), 0, 20);
+  }
+  //an arbitrary max range of 1000 is set
+  private function sanitiseNum($x) {
+    return filter_var($x, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>0, "max_range"=>1000)));
+  }
+
+  private function json_error() {
+    $msg = array("message"=>"error");
+    return json_encode($msg);
+  }
+  /**
+   * function for the schedule
+   * @param $day is an integer of the day selected, if no day is selected runs a query for all days
+   * @return string json query results
+   */
+  private function json_schedule($day = 0){
+    if($day > 0){
+      $query = "SELECT slotId, startHour,startMinute, endHour,endMinute, dayString FROM slots
+                WHERE dayInt = :dayint";
+      $day = $this->sanitiseNum($day);
+      $params = ["dayint" => $day];
+    }
+    else{
+      $query = "SELECT DISTINCT dayInt, dayString
+                FROM slots
+                ORDER BY dayInt
+                LIMIT 4";
+      $params = [];
     }
     return ($this->recordset->getJSONRecordSet($query, $params));
-}
-
-private function json_films() {
-  $query  = "SELECT title, description FROM film";
-  $params = [];
- 
-  if (isset($_REQUEST['search'])) {
-    $query .= " WHERE title LIKE :title";
-    $params = ["title" => "%".$_REQUEST['search']."%"];
-  }else {
-    if (isset($_REQUEST['id'])) {
-      $query .= " WHERE film_id = :term";
-      $term = $this->sanitiseNum($_REQUEST['id']);
-      $params = ["term" => $term];
-    }
-}
-if (isset($_REQUEST['page'])) {
-    $query .= " ORDER BY title";
-    $query .= " LIMIT 10 ";
-    $query .= " OFFSET ";
-    $query .= 10 * ($this->sanitiseNum($_REQUEST['page'])-1);
-}
+  }
+  /**
+   * function for author queries
+   * @param $name is the name of an author that has been searched if there is no name defaults to list of all authors
+   * @return string json query results 
+   */ 
+  private function json_authors($name = ""){
+  if($name != ""){
+      $query = "SELECT DISTINCT authors.name, authorInst,title, abstract, award, sessions.name FROM authors
+      INNER JOIN content_authors On authors.authorId = content_authors.authorId
+      INNER JOIN content ON content_authors.contentId = content.contentId
+      INNER JOIN sessions_content ON content.contentId = sessions_content.contentId
+      INNER JOIN sessions ON  sessions_content.sessionId = sessions.sessionId 
+      WHERE authors.name LIKE :authorname";
+      $term = $this->sanitiseString("%".$name."%");
+      $params = ["authorname" => $name];
+  }
+  else{
+      $query = "SELECT DISTINCT authors.name,authorInst FROM authors
+      INNER JOIN content_authors ON authors.authorId = content_authors.authorId
+      ORDER BY authors.name";
+      $params = [];
+  }
   return ($this->recordset->getJSONRecordSet($query, $params));
-}
+  }
 
-public function get_page() {
-  return $this->page;
-}
+  private function json_update(){
+
+  }
+  private function json_login(){
+
+  }
+
+  public function get_page() {
+    return $this->page;
+  }
 }
 ?>
---
+++ classes/router.class.php
@@ -0,0 +1,53 @@
+<?php
+/**
+* This router will return a main, documentation or about page
+* 
+* @author Alex Tuersley
+*
+*/
+class Router {
+  //Class Variables, one for the page that will be displayed and the other for the type of page
+  private $page;
+  private $type = "HTML";
+
+  /**
+     * This function runs when the class is iniated. It gets the URL and decides whether the page is a call to the api or to the web page
+ */
+ public function __construct($recordset) {
+   $url = $_SERVER["REQUEST_URI"];
+   $path = parse_url($url)['path'];
+
+   $path = str_replace(BASEPATH,"",$path);
+   $pathArr = explode('/',$path);
+   $path = (empty($pathArr[0])) ? "main" : $pathArr[0];
+
+   ($path == "api") 
+     ? $this->api_route($pathArr, $recordset) 
+     : $this->html_route($path);
+
+ }
+
+ public function api_route($pathArr, $recordset) {
+   $this->type = "JSON";
+   $this->page = new JSONpage($pathArr, $recordset);
+ }
+
+ public function html_route($path) {
+   $ini['routes'] = parse_ini_file("config/routes.ini",true);
+   $pageInfo = isset($path, $ini['routes'][$path]) 
+     ? $ini['routes'][$path] 
+     : $ini['routes']['error'];
+
+   $this->page = new WebPageWithNav($pageInfo['title'], $pageInfo['heading1'], $pageInfo['footer']);
+   $this->page->addToBody($pageInfo['text']);
+ }
+
+ public function get_type() {
+   return $this->type ; 
+ }
+
+ public function get_page() {
+   return $this->page->get_page(); 
+ }
+}
+?>
