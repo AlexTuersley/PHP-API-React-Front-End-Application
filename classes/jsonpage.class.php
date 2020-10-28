efcec25f0ebfd934b409a9324788bb0b23092538
wjpsoftwarealex <51321599+wjpsoftwarealex@users.noreply.github.com>
2020-10-28 21:41:32 +0000

    update

--- classes/jsonpage.class.php
+++ classes/jsonpage.class.php
@@ -32,7 +32,8 @@ class JSONpage {
                   $this->page = $this->json_schedule();
                 }
             }
-            elseif($pathArr[1] == "session"){
+            elseif($pathArr[1] == "presentations"){
+              //content
 
             }
             elseif($pathArr[1]== "authors"){
@@ -53,7 +54,7 @@ class JSONpage {
           
         }
         else{
-          echo "api";
+          $this->page = $this->json_api();
         }
     }
     else{
@@ -62,15 +63,34 @@ class JSONpage {
 
   }
 
-  //an arbitrary max length of 20 is set
+  //a max length of 50 is set as names will not be longer than this
   private function sanitiseString($x) {
-    return substr(trim(filter_var($x, FILTER_SANITIZE_STRING)), 0, 20);
+    return substr(trim(filter_var($x, FILTER_SANITIZE_STRING)), 0, 50);
   }
-  //an arbitrary max range of 1000 is set
+  //sanitise an integer
   private function sanitiseNum($x) {
-    return filter_var($x, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>0, "max_range"=>1000)));
+    return filter_var($x, FILTER_VALIDATE_INT);
   }
 
+  private function json_api(){
+      $api = array("message"=>"Welcome",
+                   "author"=>"Alex Tuersley",
+                    "api"=> array("/api" => "returns api endpoints and basic info",
+                                  "/api/schedule" => "returns the days of the schedule",
+                                  "/api/schedule/id" => array("returns"=> "time slots within the day", "id" => "An integer slot id that links to the times in a day"),
+                                  "/api/schedule/presentations" => "lists all presentations",
+                                  "/api/schedule/presentations/id" => array("returns" => "All presentations that have a specific slot id", "id"),
+                                  "/api/authors" => "lists all authors",
+                                  "/api/authors?search=name" => "searches for users with a name",
+                                  "/api/authors/id" => array("returns"=>"an author with all the presentations they are in and other info","id"=>"id of an author in the database"),
+                                  "/api/login" => array("returns" => "a JSON Web Token if the login is successful", "requires" => "email and password from a form"),
+                                  "/api/update" => array("returns" => "updates the title of a session id the JSON Web Token used is correct", "requires" => "JSON Web Token and the updated title of the session")
+                                  ));
+      return json_encode($api);
+  } 
+  /**
+   * returns a json encoded error message
+   */
   private function json_error() {
     $msg = array("message"=>"error");
     return json_encode($msg);
@@ -103,7 +123,7 @@ class JSONpage {
    * @return string json query results 
    */ 
   private function json_authors($id = 0){
-      if($id > 0){
+      if($id > 0){ 
           $query = "SELECT DISTINCT authors.name, authorInst,title, abstract, award, sessions.name FROM authors
           INNER JOIN content_authors On authors.authorId = content_authors.authorId
           INNER JOIN content ON content_authors.contentId = content.contentId
@@ -114,33 +134,70 @@ class JSONpage {
           $params = ["authorid" => $authorId];
       }
       else{
+          $query = "SELECT DISTINCT authors.authorId, authors.name, authorInst FROM authors
+          INNER JOIN content_authors ON authors.authorId = content_authors.authorId
+          ";
+          $params = [];
           if(isset($_REQUEST['search'])) {
-              $query = "SELECT DISTINCT authors.name, authorInst,title, abstract, award, sessions.name FROM authors
-              INNER JOIN content_authors On authors.authorId = content_authors.authorId
-              INNER JOIN content ON content_authors.contentId = content.contentId
-              INNER JOIN sessions_content ON content.contentId = sessions_content.contentId
-              INNER JOIN sessions ON  sessions_content.sessionId = sessions.sessionId 
-              WHERE authors.name LIKE :authorname";
-              $name = str_replace("%20"," ", $_REQUEST['search']);
-              $name = $this->sanitiseString("%".$name."%");
-              $params = ["authorname" => $name];
-          }
-          else{
-            $query = "SELECT DISTINCT authors.name,authorInst FROM authors
-            INNER JOIN content_authors ON authors.authorId = content_authors.authorId
-            ORDER BY authors.name";
-            $params = [];
-          }
-        
+            $query .= "WHERE authors.name LIKE :authorname";
+            $name = str_replace("%20"," ", $_REQUEST['search']);
+            $name = $this->sanitiseString("%".$name."%");
+            $params = ["authorname" => $name];
+          }        
       }
       return ($this->recordset->getJSONRecordSet($query, $params));
   }
+  private function json_presentations($id = 0){
+      if($id > 0){
 
+      }
+      else{
+
+      }
+      return ($this->recordset->getJSONRecordSet($query, $params));
+  }
   private function json_update(){
 
   }
   private function json_login(){
+    $data = json_decode(file_get_contents("php://input"));
+    $email = isset($data->email) ? filter_var($data->email,FILTER_SANITIZE_STRING,FILTER_NULL_ON_FAILURE) : null;
+    $password = isset($data->password) ? filter_var($data->password,FILTER_SANITIZE_STRING,FILTER_NULL_ON_FAILURE) : null;
+    if(!is_null($email)) {
+      $sqlQuery = "SELECT email, password FROM users WHERE email LIKE :email";
+      $params = array("email" => $email); 
+      $dbConn = pdoDB::getConnection();
+      $queryResult = $dbConn->prepare($sqlQuery);
+      $queryResult->execute($params);
+      $rows = $queryResult->fetchAll(PDO::FETCH_ASSOC);
+      $dbConn = null;
 
+      if (count($rows) > 0) {
+        if (password_verify($password, $rows[0]['password']))
+        {
+          $token = array();
+          $token['email'] = $email;
+          $token['exp'] = "hello";
+          $encodedToken = JWT::encode($token, 'secret_server_key');
+
+          http_response_code(201);
+          echo json_encode(array("message" => "User Logged in.", "token" => $encodedToken));
+        }
+        else {
+          http_response_code(201);
+          echo json_encode(array("message" => "Invalid password."));
+        }
+      }
+      else
+      {
+        http_response_code(201);
+        echo json_encode(array("message" => "Account not found."));
+      } 
+    } 
+    else{
+      http_response_code(400);
+      echo json_encode(array("message" => "Error: Data is incomplete."));
+    }
   }
 
   public function get_page() {
