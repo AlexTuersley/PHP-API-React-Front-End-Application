44b57bd4ce9c56264b8895216db20cc0103cc598
wjpsoftwarealex <51321599+wjpsoftwarealex@users.noreply.github.com>
2020-10-28 19:41:12 +0000

    Update jsonpage.class.php

--- classes/jsonpage.class.php
+++ classes/jsonpage.class.php
@@ -37,13 +37,7 @@ class JSONpage {
             }
             elseif($pathArr[1]== "authors"){
                 if(isset($pathArr[2])){
-                  if(is_numeric($pathArr[2])){
-
-                  }
-                  else{
                     $this->page = $this->json_authors($pathArr[2]);
-                  }
-                  
                 }
                 else{
                   $this->page = $this->json_authors();
@@ -104,27 +98,42 @@ class JSONpage {
   }
   /**
    * function for author queries
-   * @param $name is the name of an author that has been searched if there is no name defaults to list of all authors
+   * @param $id is the id of an author that has been selected 
+   * if a search has been run the searched name is grabbed from the url and runs a different query
    * @return string json query results 
    */ 
-  private function json_authors($name = ""){
-  if($name != ""){
-      $query = "SELECT DISTINCT authors.name, authorInst,title, abstract, award, sessions.name FROM authors
-      INNER JOIN content_authors On authors.authorId = content_authors.authorId
-      INNER JOIN content ON content_authors.contentId = content.contentId
-      INNER JOIN sessions_content ON content.contentId = sessions_content.contentId
-      INNER JOIN sessions ON  sessions_content.sessionId = sessions.sessionId 
-      WHERE authors.name LIKE :authorname";
-      $term = $this->sanitiseString("%".$name."%");
-      $params = ["authorname" => $name];
-  }
-  else{
-      $query = "SELECT DISTINCT authors.name,authorInst FROM authors
-      INNER JOIN content_authors ON authors.authorId = content_authors.authorId
-      ORDER BY authors.name";
-      $params = [];
-  }
-  return ($this->recordset->getJSONRecordSet($query, $params));
+  private function json_authors($id = 0){
+      if($id > 0){
+          $query = "SELECT DISTINCT authors.name, authorInst,title, abstract, award, sessions.name FROM authors
+          INNER JOIN content_authors On authors.authorId = content_authors.authorId
+          INNER JOIN content ON content_authors.contentId = content.contentId
+          INNER JOIN sessions_content ON content.contentId = sessions_content.contentId
+          INNER JOIN sessions ON  sessions_content.sessionId = sessions.sessionId 
+          WHERE authors.authorId = :authorid";
+          $authorId = $this->sanitiseNum($id);
+          $params = ["authorid" => $authorId];
+      }
+      else{
+          if(isset($_REQUEST['search'])) {
+              $query = "SELECT DISTINCT authors.name, authorInst,title, abstract, award, sessions.name FROM authors
+              INNER JOIN content_authors On authors.authorId = content_authors.authorId
+              INNER JOIN content ON content_authors.contentId = content.contentId
+              INNER JOIN sessions_content ON content.contentId = sessions_content.contentId
+              INNER JOIN sessions ON  sessions_content.sessionId = sessions.sessionId 
+              WHERE authors.name LIKE :authorname";
+              $name = str_replace("%20"," ", $_REQUEST['search']);
+              $name = $this->sanitiseString("%".$name."%");
+              $params = ["authorname" => $name];
+          }
+          else{
+            $query = "SELECT DISTINCT authors.name,authorInst FROM authors
+            INNER JOIN content_authors ON authors.authorId = content_authors.authorId
+            ORDER BY authors.name";
+            $params = [];
+          }
+        
+      }
+      return ($this->recordset->getJSONRecordSet($query, $params));
   }
 
   private function json_update(){
